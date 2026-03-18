7638727e21f4b8c0b78b99ca6f3aa705c5b97737
Acrsahil <acharyasahil2003@gmail.com>
2026-03-18 23:29:19 +0545

    fix: url

--- src/api/config.js
+++ src/api/config.js
@@ -4,7 +4,8 @@
  */
 
 // const RAW_BASE = import.meta.env.VITE_API_BASE_URL || "https://amabakerypos-production.up.railway.app";
-const RAW_BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+const RAW_BASE = import.meta.env.VITE_API_BASE_URL || "https://api.amabakeryhouse.com";
+// const RAW_BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
 
 
 
